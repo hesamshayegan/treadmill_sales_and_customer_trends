1c1f36d78a74bd26b7e8be33ac50378b3179e957
Hesam <hesamshayegan88@gmail.com>
2025-02-20 16:18:01 -0800

    added tableau dashboard + README.md

---
+++ README.md
@@ -0,0 +1,72 @@
+# Treadmill Product Data Analysis
+
+## Overview
+
+This repository contains an analysis of treadmill product data using PostgreSQL for data processing and Tableau for data visualization. The goal of this analysis was to uncover insights into customer demographics, fitness levels, and treadmill usage patterns to inform business and marketing strategies.
+
+## Key Insights
+
+### Fitness Levels and Treadmill Usage
+
+- KP781 Users Achieve Highest Fitness Levels: The KP781 treadmill is used by individuals with the widest range of fitness levels, achieving the highest maximum fitness level among all models.
+
+- KP481 Caters to Lower Fitness Ranges: The KP481 model is primarily used by individuals at the lower end of the fitness spectrum, making it a preferred choice for beginners or those with lower fitness goals.
+
+Treadmill Model Aligns with Fitness Level:
+
+- KP481 users have the lowest fitness levels.
+
+- KP281 users fall in the mid-range.
+
+- KP781 users have the highest fitness levels.
+
+### Demographics and Customer Behavior
+
+KP281 Attracts Younger Customers with Moderate Usage: The KP281 treadmill has the youngest average customer age (28.6 years) and moderate average mileage (82.79 miles), indicating a preference among younger users for regular, but not excessive, workouts.
+
+KP481 Users Maintain Consistent Mileage with Slightly Older Age: The KP481 treadmill users have a slightly older average age (28.9 years) and maintain a steady average mileage (87.93 miles).
+
+### Customer Count and Total Mileage
+
+#### High Customer Count Drives Significant Total Mileage:
+
+The KP281 has the highest customer count (80), resulting in a substantial total mileage of 6.6K.
+
+The KP481 has a lower customer count (60), leading to a smaller total mileage of 5.3K.
+
+### Income and Treadmill Preferences
+
+#### Income Drives Treadmill Choice:
+
+- Higher-income customers prefer the premium KP781 model.
+
+- Lower-income customers favor the budget-friendly KP481.
+
+- The KP281 falls between the two in terms of both price and user demographics.
+
+Positive Income-Fitness Link: Higher-income individuals exhibit a stronger correlation between income and fitness level, reinforcing the link between financial status and treadmill selection.
+
+## Business Recommendations
+
+### Targeted Marketing is Key:
+
+- KP781: Market with premium messaging targeting affluent customers. (more popular among male users)
+
+- KP281: Highlight value and versatility to appeal to mid-range buyers.
+
+- KP481: Emphasize affordability and accessibility for budget-conscious consumers.
+
+Further Analysis: Exploring additional factors such as demographics, usage patterns, and fitness goals can refine customer segmentation and enhance product development strategies.
+
+## Tools Used
+
+PostgreSQL: Data processing and analysis
+
+Tableau: Data visualization and reporting
+
+ 
+
+[Tableau Dashboard](https://public.tableau.com/views/treadmill_analysis/Dashboard?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link)
+
+
+![Tableau Dashboard](results/Treadmill_Usage_&_Fitness_Insights.png "tableau_dashboard")
--- treadmill_product_trends.sql
+++ treadmill_product_trends.sql
@@ -2,7 +2,7 @@
 \c treadmill_db;
 
 -- Drop the treadmill_products if already exists
-DROP TABLE IF EXISTS treadmill_products;
+DROP TABLE IF EXISTS treadmill_products CASCADE;
 
 -- Create the treadmill_products table
 CREATE TABLE treadmill_products (
@@ -20,36 +20,83 @@ CREATE TABLE treadmill_products (
 
 -- Import the input CSV file
 COPY treadmill_products(Product, Age, Gender, Education, MaritalStatus, Usage, Fitness, Income, Miles)
-FROM '/treadmill_sales_and_customer_trends/treadmill_data.csv'
+FROM '/Users/hesam/Desktop/codes/treadmill_sales_and_customer_trends/input/treadmill_data.csv'
 DELIMITER ','
 CSV HEADER;
 
 -- Average age of customer per Treadmill Type
-SELECT Product, AVG(Age) AS average_age FROM treadmill_products
-GROUP BY Product;
-
 -- Count of customers per Treadmill Type
-SELECT Product, COUNT(*) AS total_customers FROM treadmill_products
-GROUP BY Product
-ORDER BY total_customers DESC;
-
 -- Average miles run per Treadmill Type
-SELECT Product, AVG(Miles) AS average_miles FROM treadmill_products
-GROUP BY Product
-ORDER BY average_miles DESC;
-
 --  Range of fitness levels associated with each Treadmill Type
-SELECT Product, MIN(Fitness) AS min_level_fitness, MAX(Fitness) AS max_level_fitness FROM treadmill_products
-GROUP BY Product;
-
--- Fitness level distribution per Treadmill Type
-SELECT Product, Fitness, COUNT(*) FROM treadmill_products
-GROUP BY Product, Fitness
-ORDER BY Product, Fitness;
-
 -- The correlation between income and fitness level for each treadmill type
-SELECT Product, CORR(Fitness, Income) FROM treadmill_products
-GROUP BY Product;
+-- Treadmill type associated with the highest income customers
+-- Top 3 treadmill types used by the most active customers (highest miles run)
+
+COPY(
+    WITH 
+        avg_age AS (
+            SELECT Product, AVG(Age) AS average_age 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        customer_count AS (
+            SELECT Product, COUNT(*) AS total_customers 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        avg_miles AS (
+            SELECT Product, AVG(Miles) AS average_miles 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        fitness_range AS (
+            SELECT Product, MIN(Fitness) AS min_level_fitness, MAX(Fitness) AS max_level_fitness 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        income_correlation AS (
+            SELECT Product, CORR(Fitness, Income) AS income_fitness_correlation 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        high_income AS (
+            SELECT Product, PERCENTILE_CONT(0.9) WITHIN GROUP (ORDER BY Income) AS high_income_percentile 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        max_income AS (
+            SELECT Product, MAX(Income) AS max_income 
+            FROM treadmill_products 
+            GROUP BY Product
+        ),
+        top_miles AS (
+            SELECT Product, SUM(Miles) AS total_miles 
+            FROM treadmill_products 
+            GROUP BY Product
+        )
+
+    SELECT
+            a.Product, 
+            a.average_age, 
+            c.total_customers, 
+            m.average_miles, 
+            f.min_level_fitness, 
+            f.max_level_fitness, 
+            ic.income_fitness_correlation, 
+            hi.high_income_percentile, 
+            mi.max_income, 
+            tm.total_miles
+        FROM avg_age a
+        LEFT JOIN customer_count c ON a.Product = c.Product
+        LEFT JOIN avg_miles m ON a.Product = m.Product
+        LEFT JOIN fitness_range f ON a.Product = f.Product
+        LEFT JOIN income_correlation ic ON a.Product = ic.Product
+        LEFT JOIN high_income hi ON a.Product = hi.Product
+        LEFT JOIN max_income mi ON a.Product = mi.Product
+        LEFT JOIN top_miles tm ON a.Product = tm.Product
+    ORDER BY c.total_customers DESC
+)
+TO '/Users/hesam/Desktop/codes/treadmill_sales_and_customer_trends/results/treadmill_summary.csv' WITH (FORMAT CSV, HEADER);
 
 -- Gender preference distribution across treadmill types (using CTE)
 -- WITH ProductCounts AS (
@@ -67,9 +114,21 @@ GROUP BY Product;
 -- ORDER BY sales.Product;
 
 -- Gender preference distribution across treadmill types (using window function - Over clause)
-SELECT sales.Product,
-       sales.Gender,
-       ROUND(COUNT(sales.Gender) * 100.0 / SUM(COUNT(sales.Gender)) OVER (PARTITION BY sales.Product), 2) AS gender_percentage
-    FROM treadmill_products AS sales
-    GROUP BY sales.Product, sales.Gender
-ORDER BY sales.Product;
+COPY(
+    SELECT sales.Product,
+        sales.Gender,
+        ROUND(COUNT(sales.Gender) * 100.0 / SUM(COUNT(sales.Gender)) OVER (PARTITION BY sales.Product), 2) AS gender_percentage
+        FROM treadmill_products AS sales
+        GROUP BY sales.Product, sales.Gender
+    ORDER BY sales.Product
+)
+TO '/Users/hesam/Desktop/codes/treadmill_sales_and_customer_trends/results/treadmill_gender_percentage.csv' WITH (FORMAT CSV, HEADER);
+
+-- Fitness level distribution per Treadmill Type
+COPY(
+    SELECT Product, Fitness, COUNT(*)
+        FROM treadmill_products
+        GROUP BY Product, Fitness
+    ORDER BY Product, Fitness
+)
+TO '/Users/hesam/Desktop/codes/treadmill_sales_and_customer_trends/results/treadmill_fitness_level.csv' WITH (FORMAT CSV, HEADER);
